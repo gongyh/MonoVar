76b2a886cf22c917b7cdae783947e74c54f36bcb
gongyh <gongyh@qibebt.ac.cn>
2021-03-16 10:43:49 +0800

    fix setup.py script

--- setup.py
+++ setup.py
@@ -17,35 +17,31 @@ except ImportError:
 BASEPATH=os.path.dirname(os.path.abspath(__file__))
 
 class MonoVarBuild(build):
-
-	def run(self):
-		# run original build code
-		build.run(self)
-
-		# build samtools
-		build_path = os.path.abspath(self.build_temp)
-		cmd = ['make', '-C', 'external/samtools']
-
-		def compile():
-			subprocess.check_call(cmd)
-
-		self.execute(compile, [], 'Compile samtools')
+    def run(self):
+        # run original build code
+        build.run(self)
+        # build samtools
+        build_path = os.path.abspath(self.build_temp)
+        cmd = ['make', '-C', 'external/samtools']
+        def compile():
+            subprocess.check_call(cmd)
+        self.execute(compile, [], 'Compile samtools')
 
 
 class MonoVarInstall(install):
-	def run(self):
-		install.run(self)
-		shutil.copy2('external/samtools/samtools', \
+    def run(self):
+        install.run(self)
+        shutil.copy2('external/samtools/samtools', \
             os.path.join(self.install_lib, 'monovar_src'))
 
 
 def main():
     py_v = float(sys.version[:3])
-	if py_v < 2.6 or py_v >= 2.8:
-    	sys.stderr.write("CRITICAL: Python version must be 2.6 or 2.7!\n")
-    	sys.exit(1)
+    if py_v < 2.7:
+        sys.stderr.write("CRITICAL: Python version must be >= 2.7!\n")
+        sys.exit(1)
 
-	setup(name="MonoVar", author="Hamim Zafar",
+    setup(name="MonoVar", author="Hamim Zafar",
         cmdclass={'build': MonoVarBuild, 'install': MonoVarInstall})
 
 if __name__ == '__main__':
